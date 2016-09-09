56ef797e4d9f92bd3b8b07c7da1900b5dc446393
softwarenacho <trsgarp@gmail.com>
2016-09-09 11:34:16 -0500

    Added phone params on Register Demo link

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,23 @@
 class ContactsController < ApplicationController
 
 	def advance_form
-		p "Trying console print"
 		@contact = Contact.new
-		# if params[:id]
-			pp @id = params[:id]
-		# else
+		if params[:id]
+			@id = params[:id]
+		else
 		# 	# request = URI.parse("https://crm.zoho.com/crm/private/json/Leads/searchRecords?authtoken=#{ENV['ZOHO_TOKEN']}&scope=crmapi&criteria=(Email:#{params[:email]})")
 		# 	# pp response = JSON.parse(Net::HTTP.get(request))
 
 		# 	# @id = response["response"]["result"]["Leads"]["row"]["FL"][0]["content"]
-		# 	pp @id = false
-		# end
+			@id = false
+		end
 	end
 
   def update_zoho
     base_request = "https://crm.zoho.com/crm/private/json/Leads/updateRecords?authtoken=#{ENV['ZOHO_TOKEN']}&scope=crmapi&id=#{params[:contact][:zoho_id]}&xmlData="
     changes = ""
-    phone = params[:contact][:phone]
-    params[:contact][:phone] = (phone == nil || (phone.scan(/\d+/).join().size < 7) || (phone.scan(/\d+/).join().size > 16)) ? phone.scan(/\d+/).join() : ""
+    @phone = params[:contact][:phone]
+    params[:contact][:phone] = (@phone == nil || (@phone.scan(/\d+/).join().size < 7) || (@phone.scan(/\d+/).join().size > 16)) ? @phone.scan(/\d+/).join() : ""
     params[:contact].each do |k,v|
       if v != "" && k != :zoho_id.to_s
         field = k.capitalize.dup
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root 'contacts#advance_form'
-  get 'static', to: 'contacts#test'
   post 'advance_form', to: 'contacts#update_zoho'
 end
